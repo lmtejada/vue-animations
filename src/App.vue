aa6a25c33de9924c4fc7f138540a2832c139918d
lmtejada <laura_mtc@hotmail.com>
2019-05-14 18:40:19 -0500

    Animations and transitions when adding or removing dom elements

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,26 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
-
+                <hr>
+                <select v-model="alertAnimation" class="form-control">
+                    <option value="fade">Fade</option>
+                    <option value="slide">Slide</option>
+                </select>
+                <br><br>
+                <button class="btn btn-primary" @click="show = !show">Show alert</button>
+                <br><br>
+                <transition :name="alertAnimation" type="animation">
+                    <div class="alert alert-info" v-show="show">This is some info</div>
+                </transition>
+                <transition name="slide" type="animation" appear>
+                    <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
+                <transition
+                    enter-active-class="animated bounce"
+                    leave-active-class="animated shake"
+                >
+                    <div class="alert alert-info" v-if="show">This is some info</div>
+                </transition>
             </div>
         </div>
     </div>
@@ -13,12 +32,67 @@
     export default {
         data() {
             return {
-
+                show: true,
+                alertAnimation: 'fade'
             }
         }
     }
 </script>
 
 <style>
+    /* This is only attached for one frame at the beginning, this class will be inmmediately removed after one frame */
+    .fade-enter {
+        opacity: 0;
+    }
 
+    .fade-enter-active {
+        transition: opacity 1s;
+    }
+
+    .fade-leave {
+        /* opacity: 1; */
+    }
+
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    .slide-enter {
+        opacity: 0;
+        /* transform: translateY(20px); */
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+        transition: opacity .5s;
+    }
+
+    .slide-leave {
+
+    }
+
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+        transition: opacity 1s;
+        opacity: 0;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
+    }
 </style>
